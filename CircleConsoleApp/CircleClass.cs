22df252582c6d6e97bf6fe25b860e62b44a43f88
fahin <fahins@yahoo.com>
2014-12-25 23:05:36 +0600

    circle

---
+++ CircleConsoleApp/CircleClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleConsoleApp
+{
+    class CircleClass
+    {
+        public double radius;
+        const double pi = 3.1416;
+        public double diameteFunction()
+        {
+
+            return (2 * radius);
+
+        }
+
+        public double perimeterFunction()
+        {
+            return (2 * pi * radius);
+        }
+
+        public double areaFunction()
+        {
+            return (pi * radius * radius);
+        }
+    }
+}
---
+++ CircleConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircleClass circle = new CircleClass();
+            Console.WriteLine("Enter radius Value\n");
+            circle.radius= Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Diamter is "+circle.diameteFunction()+"\n Perimeter is "+ circle.perimeterFunction()+"\n area is :"+ circle.areaFunction());
+
+            Console.ReadKey();
+        }
+    }
+}
